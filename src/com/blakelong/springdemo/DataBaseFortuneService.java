1e4ce287cd0f368ff84bdf565aaafc9685cfe085
Blake A. Long <blake.a.long@gmail.com>
2020-09-18 19:41:43 -0600

    additional FortuneService implementations

---
+++ src/com/blakelong/springdemo/DataBaseFortuneService.java
@@ -0,0 +1,11 @@
+package com.blakelong.springdemo;
+
+public class DataBaseFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
